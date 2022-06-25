b481db4c618e141575b98d79eb736c269cecf764
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-06-25 05:31:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
   let plot4 = 0;
   document.getElementById('parkAreaChose').addEventListener("change", function() {
   if (this.value == "1") {
-              plot1 = 0;
+              // plot1 = 0;
               const dbRef = ref(getDatabase());
                       get(child(dbRef, 'plot1/')).then((snapshot) => {
                         if (snapshot.exists()) {
@@ -71,3 +71,141 @@ const firebaseConfig = {
      document.getElementById("slotFree").innerHTML = '0';
   }
 });
+const html5QrCode = new Html5Qrcode("reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    if(decodedText=="Parking A"){
+                const dbRef = ref(getDatabase());
+                        get(child(dbRef, 'plot1/')).then((snapshot) => {
+                          if (snapshot.exists()) {
+                            plot1 = snapshot.val();
+                          } else {
+                            console.log("No data available");
+                          }
+                        }).catch((error) => {
+                          console.error(error);
+                        });
+                if(plot1==1) {
+                     myFunction1();
+                }else{
+                        get(child(dbRef, 'plot2/')).then((snapshot) => {
+                          if (snapshot.exists()) {
+                            plot2 = snapshot.val();
+                              // document.getElementById("slotFree").innerHTML = (plot1+plot2).toString();
+                          } else {
+                            console.log("No data available");
+                          }
+                        }).catch((error) => {
+                          console.error(error);
+                        });
+                        if(plot2==1){
+                          myFunction2();
+                        }else{
+                          myFunctionNo();
+                        }
+                      }
+    }
+    if(decodedText=="Parking B"){
+      const dbRef = ref(getDatabase());
+              get(child(dbRef, 'plot3/')).then((snapshot) => {
+                if (snapshot.exists()) {
+                  plot3 = snapshot.val();
+                } else {
+                  console.log("No data available");
+                }
+              }).catch((error) => {
+                console.error(error);
+              });
+      if(plot3==1) {
+           myFunction1();
+      }else{
+              get(child(dbRef, 'plot4/')).then((snapshot) => {
+                if (snapshot.exists()) {
+                  plot4 = snapshot.val();
+                    // document.getElementById("slotFree").innerHTML = (plot1+plot2).toString();
+                } else {
+                  console.log("No data available");
+                }
+              }).catch((error) => {
+                console.error(error);
+              });
+              if(plot4==1){
+                myFunction2();
+              }else{
+                myFunctionNo();
+              }
+            }
+    }
+};
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+// If you want to prefer back camera
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+// Select back camera or fail with `OverconstrainedError`.
+html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
+
+function myFunction1() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar1");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+}
+function myFunction2() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar2");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+}
+function myFunction3() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar3");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+}
+function myFunction4() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar4");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+}
+function myFunctionNo() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar5");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+}
+
+document.getElementById('bookBT').addEventListener('click',sendEmail());
+function sendEmail() {
+Email.send({
+    Host : "smtp.mailtrap.io",
+    Username : "2c12f875aec83b",
+    Password : "4223c16385e7a1",
+    To : 'bharatdeep.work@gmail.com',
+    From : "bharatdeep.race@gmail.com",
+    Subject : "Booking Confirmed",
+    Body : "Booking is confirmed, Thank you for choosing Park-o-pedia"
+}).then(
+  message => alert(message)
+);
+}
